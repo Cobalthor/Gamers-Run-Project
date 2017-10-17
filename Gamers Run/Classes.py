d2e2fb28c99eb089377dbc1bb5f4a95db765c2a8
Cobalthor <31163658+Cobalthor@users.noreply.github.com>
2017-10-16 20:10:27 -0400

    Cache des curseurs inutiles

---
+++ Gamers Run/Classes.py
@@ -0,0 +1,107 @@
+from Constantes import *
+
+#Classe gérant les curseurs de l'écran de sélection des personnages
+
+
+class Curseur:
+
+    """Classe permettant de créer un curseur"""
+    def __init__(self):
+        #Position du curseur en cases et en pixels
+        self.perso_x = 0
+        self.perso_y = 0
+        self.x = 50
+        self.y = 155
+        self.perso = Donkey_Kong
+
+    def deplacer(self, direction):
+        """Methode permettant de déplacer la cible"""
+        #Déplacement vers la droite
+        if direction == 'droite':
+            if self.perso_y <= 1:
+                if self.perso_x != 4:
+                    self.perso_x += 1
+                    self.x += 85
+            elif self.perso_y == 2:
+                if self.perso_x != 1:
+                    self.perso_x += 1
+                    self.x += 85
+
+        #Déplacement vers la gauche
+        if direction == 'gauche':
+            if self.perso_y <= 1:
+                if self.perso_x != 0:
+                    self.perso_x -= 1
+                    self.x -= 85
+            elif self.perso_y == 2:
+                if self.perso_x != 0:
+                    self.perso_x -= 1
+                    self.x -= 85
+
+        #Déplacement vers le haut
+        if direction == 'haut':
+            if self.perso_y != 0:
+                if self.perso_y != 2:
+                    self.perso_y -= 1
+                    self.y -= 68
+                else:
+                    if self.perso_x == 0:
+                        self.perso_y = 1
+                        self.perso_x = 1
+                        self.y -= 68
+                        self.x = 135
+                    elif self.perso_x == 1:
+                        self.perso_y = 1
+                        self.perso_x = 3
+                        self.y -= 68
+                        self.x = 305
+
+        #Déplacement vers le bas
+        if direction == 'bas' != 7:
+            if self.perso_y != 2:
+                if self.perso_y == 0:
+                    self.perso_y += 1
+                    self.y += 68
+                elif self.perso_y == 1:
+                    if self.perso_x <= 2:
+                        self.perso_y += 1
+                        self.perso_x = 0
+                        self.y += 68
+                        self.x = 177
+                    if self.perso_x >= 3:
+                        self.perso_y += 1
+                        self.perso_x = 1
+                        self.y += 68
+                        self.x = 262
+
+    def placer(self):
+        #Fonction déterminant le personnage sélectionné
+        if self.perso_x == 0:
+            if self.perso_y == 0:
+                self.perso = Donkey_Kong
+            if self.perso_y == 1:
+                self.perso = Pacman
+            if self.perso_y == 2:
+                self.perso = Sonic
+        if self.perso_x == 1:
+            if self.perso_y == 0:
+                self.perso = Kirby
+            if self.perso_y == 1:
+                self.perso = Pikachu
+            if self.perso_y == 2:
+                self.perso = Yoshi
+        if self.perso_x == 2:
+            if self.perso_y == 0:
+                self.perso = Link
+            if self.perso_y == 1:
+                self.perso = Pit
+        if self.perso_x == 3:
+            if self.perso_y == 0:
+                self.perso = Mario
+            if self.perso_y == 1:
+                self.perso = Samus
+        if self.perso_x == 4:
+            if self.perso_y == 0:
+                self.perso = Mega_Man
+            if self.perso_y == 1:
+                self.perso = Shovel_Knight
---
+++ Gamers Run/Constantes.py
@@ -0,0 +1,64 @@
+import pygame
+
+pygame.init()
+
+continuer_selec = 1
+
+titre_fenetre = 'Gamers Run'
+
+#IMPORTATION DES IMAGES
+
+#Écran de sélection des personnages
+
+#Fond
+fond_selec = 'Fond 3.jpg'
+toads = 'Toads.png'
+retour = 'Retour.png'
+#Personnages
+donkey_kong = 'donkey kong.png'
+kirby = 'kirby.png'
+link = 'link.png'
+mario = 'mario.png'
+mega_man = 'Mega_Man.png'
+pacman = 'Pacman.png'
+pikachu = 'pikachu.png'
+pit = 'Pit.png'
+samus = 'samus.png'
+shovel_knight = 'Shovel_Knight.png'
+sonic = 'Sonic.png'
+yoshi = 'yoshi.png'
+#Curseurs
+curseur_1 = 'curseur_J1.png'
+curseur_2 = 'curseur_J2.png'
+curseur_3 = 'curseur_J3.png'
+#Joueurs
+j1 = 'J1.png'
+j2 = 'J2.png'
+j3 = 'J3.png'
+
+
+#CONVERSION DES IMAGES
+
+#Images de l'écran de sélections des personnages
+
+Fond_2 = pygame.image.load(fond_selec).convert_alpha()
+Toads = pygame.image.load(toads).convert_alpha()
+Retour = pygame.image.load(retour).convert_alpha()
+Donkey_Kong = pygame.image.load(donkey_kong).convert_alpha()
+Kirby = pygame.image.load(kirby).convert_alpha()
+Link = pygame.image.load(link).convert_alpha()
+Mario = pygame.image.load(mario).convert_alpha()
+Mega_Man = pygame.image.load(mega_man).convert_alpha()
+Pacman = pygame.image.load(pacman).convert_alpha()
+Pikachu = pygame.image.load(pikachu).convert_alpha()
+Pit = pygame.image.load(pit).convert_alpha()
+Samus = pygame.image.load(samus).convert_alpha()
+Shovel_Knight = pygame.image.load(shovel_knight).convert_alpha()
+Sonic = pygame.image.load(sonic).convert_alpha()
+Yoshi = pygame.image.load(yoshi).convert_alpha()
+Curseur_1 = pygame.image.load(curseur_1).convert_alpha()
+Curseur_2 = pygame.image.load(curseur_2).convert_alpha()
+Curseur_3 = pygame.image.load(curseur_3).convert_alpha()
+J1 = pygame.image.load(j1).convert_alpha()
+J2 = pygame.image.load(j2).convert_alpha()
+J3 = pygame.image.load(j3).convert_alpha()
